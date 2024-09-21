53c4a9cf881f556c9a274b18c7e9a945250ab66d
HiramZednem <hirammendez000@gmail.com>
2024-09-21 15:15:27 -0600

    FIX: bug that response with res.status 201 when get user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -14,7 +14,7 @@ export const userController = {
     getById: async (req: Request, res: Response) => {
         try {
             const user = await userService.getById(req.params.id);
-            return res.status(201).json(user);
+            return res.status(200).json(user);
         } catch (e) {
             res.status(500).json({ error: (e as Error).message });
         }
@@ -22,7 +22,7 @@ export const userController = {
     create: async (req: Request, res: Response) => {
         try {
             const user = await userService.create(req.body);
-            return res.status(200).json(user);
+            return res.status(201).json(user);
         } catch (e) {
             res.status(500).json({ error: (e as Error).message });
         }
