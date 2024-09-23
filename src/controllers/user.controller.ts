81f26d7b8d5cef9429cdf8d86ce7464503cacd88
HiramZednem <hirammendez000@gmail.com>
2024-09-23 00:06:52 -0600

    ADD: user login implemented and password hashed

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { userService } from '../services';
-
+import bcrypt, { hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { JWT_KEY } from '../config';
 
 export const userController = {
     getAll: async (req: Request, res: Response) => {
@@ -21,7 +23,8 @@ export const userController = {
     },
     create: async (req: Request, res: Response) => {
         try {
-            const user = await userService.create(req.body);
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const user = await userService.create({...req.body, password: hashedPassword});
             return res.status(201).json(user);
         } catch (e) {
             res.status(500).json({ error: (e as Error).message });
@@ -42,6 +45,26 @@ export const userController = {
         } catch (e) {
             res.status(500).json({ error: (e as Error).message });
         }
-    }
+    },
+    login: async (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body;
+            const user = await userService.getUserByEmail(email);
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if (!passwordMatch) {
+                return res.status(401).json({ error: 'Invalid password' });
+            }
+
+
+            const token = jwt.sign({ user }, JWT_KEY as string, { expiresIn: "12h" });
+
+            return res.status(200).json({ user, token });
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to login' });
+        }
+    },
 
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,5 +8,6 @@ router.get('/:id', userController.getById);
 router.post('/', userController.create);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
+router.post('/login', userController.login);
 
 export default router;
