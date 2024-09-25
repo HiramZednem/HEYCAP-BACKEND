bee5d51e72190e3577034e6f49bd243c0c3a6671
HiramZednem <hirammendez000@gmail.com>
2024-09-24 18:15:10 -0600

    fix: improve the names of intinerary routes

--- src/routes/itinerary.routes.ts
+++ src/routes/itinerary.routes.ts
@@ -4,9 +4,9 @@ import { itineraryController } from '../controllers';
 
 const router = Router();
 
-router.get('/user/:userid', itineraryController.getAll);
+router.get('/users/:userid', itineraryController.getAll);
 router.get('/:id',  itineraryController.getById);
-router.post('/:userid', itineraryController.create);
+router.post('/users/:userid', itineraryController.create);
 router.put('/:id',  itineraryController.update);
 router.delete('/:id',  itineraryController.delete);
 
