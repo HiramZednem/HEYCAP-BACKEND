9f348d92673d67317ee6ce8a5f0f6bdf3b09174d
HiramZednem <hirammendez000@gmail.com>
2024-10-21 00:07:08 -0600

    ADD: returning places

--- src/controllers/place.controller.ts
+++ src/controllers/place.controller.ts
@@ -1,76 +1,132 @@
 import { Request, Response } from "express";
 import { PlaceService } from "../services/place.service";
 import { BaseResponse } from "../dtos/base.response";
+import { jwtPlugin } from "../public";
+import { tokenService } from "../services/token.service";
+import { GoogleService } from "../services";
 
 export class PlaceController {
     private placeServices: PlaceService;
+    private googleServices: GoogleService;
 
     constructor(){
         this.placeServices = new PlaceService();
+        this.googleServices = new GoogleService();
     }
 
-    public async getPlaces(req: Request, res: Response) {
+    // public async getPlaces(req: Request, res: Response) {
+    //     try {
+    //         const result = await this.placeServices.getAllPlaces();
+    //         const response = new BaseResponse(result, true, 'Places found');
+    //         res.status(200).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         const response = new BaseResponse({}, false, 'Error getting places');
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+    // }
+
+    // public async getPlacesInOrder(req: Request, res: Response) {
+    //     try {
+    //         const { per_page=9, page=1 } = req.query
+    //         const result = await this.placeServices.getPlacesByPage(parseInt(per_page as string), parseInt(page as string));
+    //         const response = new BaseResponse(result, true, 'Places found');
+    //         res.status(200).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         const response = new BaseResponse({}, false, 'Error getting places per page');
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+    // }
+
+    // public async getPlaceByiD(req: Request, res: Response) {
+    //     try {
+    //         const { id_place } = req.params;
+    //         const result = await this.placeServices.getPlaceById(parseInt(id_place));
+
+    //         if(!result) {
+    //             const response = new BaseResponse(result, true, 'No places added yet');
+    //             return res.status(200).json(response.toResponseEntity());
+    //         }
+    //         const response = new BaseResponse(result, true, "Place Found");
+    //         res.status(200).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         const response = new BaseResponse({}, false, 'Error getting place');
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+    // }
+
+    // public async registerPlace(req: Request, res: Response) {
+    //     try {
+    //         const { name } = req.body;
+    //         // const result = await this.placeServices.registerPlace({ name: name});
+    //         // const response = new BaseResponse(result, true, 'Place created');
+    //         // res.status(201).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         console.log(error);
+    //         const response = new BaseResponse(error, false, 'Error creating place');
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+    // }
+
+    // public async deletePlace(req: Request, res: Response) {
+    //     try {
+    //         const { id_place } = req.params;
+    //         const result = await this.placeServices.deletePlace(parseInt(id_place));
+    //         const response = new BaseResponse(result, true, 'Place deleted');
+    //         res.status(200).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         const response = new BaseResponse({}, false, "Error deleting place");
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+    // }
+
+    // // by id
+    // public async get(req: Request, res: Response) {
+    //     try {
+    //         const { id_place } = req.params;
+    //         const result = await this.placeServices.getPlaceById(parseInt(id_place));
+
+    //         if(!result) {
+    //             const response = new BaseResponse(result, true, 'No places added yet');
+    //             return res.status(200).json(response.toResponseEntity());
+    //         }
+    //         const response = new BaseResponse(result, true, "Place Found");
+    //         res.status(200).json(response.toResponseEntity());
+    //     } catch (error) {
+    //         const response = new BaseResponse({}, false, 'Error getting place');
+    //         res.status(500).json(response.toResponseEntity());
+    //     }
+
+    // }
+
+    public async getNearbyPlaces(req: Request, res: Response) {
         try {
-            const result = await this.placeServices.getAllPlaces();
-            const response = new BaseResponse(result, true, 'Places found');
-            res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, 'Error getting places');
-            res.status(500).json(response.toResponseEntity());
+        // const accessToken = req.app.locals.accessToken;
+        // const uuid = jwtPlugin.decode(accessToken).uuid;
+        // await tokenService.validateToken(accessToken, uuid);
+
+        const { lat, lng, next_page_token } = req.body;
+
+        const places = await this.googleServices.getNearbyPlaces(lat, lng, next_page_token);
+        
+        const response = new BaseResponse(places, true, "Nearby places found");
+        res.status(200).json(response.toResponseEntity());
+        // recibo mis places
+        // const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&pagetoken=${next_page_token}&key=${apiKey}`;
+        // de los places que me llegaron los voy a filtrar si existen en mi bd por el google_id
+        // si existe next, si no existe lo guardo en mi bd
+
+        // De los places que me llegaron, voy a filtrar si el usuario les dio like o dislike y esos voy a regresar junto con el next page
+
+        } catch {
+
         }
+        
     }
 
-    public async getPlacesInOrder(req: Request, res: Response) {
-        try {
-            const { per_page=9, page=1 } = req.query
-            const result = await this.placeServices.getPlacesByPage(parseInt(per_page as string), parseInt(page as string));
-            const response = new BaseResponse(result, true, 'Places found');
-            res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, 'Error getting places per page');
-            res.status(500).json(response.toResponseEntity());
-        }
+    public async likePlace(req: Request, res: Response) {
     }
 
-    public async getPlaceByiD(req: Request, res: Response) {
-        try {
-            const { id_place } = req.params;
-            const result = await this.placeServices.getPlaceById(parseInt(id_place));
-
-            if(!result) {
-                const response = new BaseResponse(result, true, 'No places added yet');
-                return res.status(200).json(response.toResponseEntity());
-            }
-            const response = new BaseResponse(result, true, "Place Found");
-            res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, 'Error getting place');
-            res.status(500).json(response.toResponseEntity());
-        }
+    public async dislikePlace(req: Request, res: Response) {
     }
 
-    public async registerPlace(req: Request, res: Response) {
-        try {
-            const { name } = req.body;
-            const result = await this.placeServices.registerPlace({ name: name});
-            const response = new BaseResponse(result, true, 'Place created');
-            res.status(201).json(response.toResponseEntity());
-        } catch (error) {
-            console.log(error);
-            const response = new BaseResponse(error, false, 'Error creating place');
-            res.status(500).json(response.toResponseEntity());
-        }
-    }
-
-    public async deletePlace(req: Request, res: Response) {
-        try {
-            const { id_place } = req.params;
-            const result = await this.placeServices.deletePlace(parseInt(id_place));
-            const response = new BaseResponse(result, true, 'Place deleted');
-            res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, "Error deleting place");
-            res.status(500).json(response.toResponseEntity());
-        }
-    }
 }
--- src/services/place.service.ts
+++ src/services/place.service.ts
@@ -1,5 +1,6 @@
 import { Prisma, places } from "@prisma/client";
 import { prisma } from "../db/db";
+import { PlaceResponse } from "../dtos/place.interface";
 
 interface Place {
     place_id: number;
@@ -18,77 +19,95 @@ type PlacesResponse = {
 export class PlaceService {
     constructor() { }
 
+    // public async getAllPlaces(): Promise<places[]> {
+    //     return await prisma.places.findMany({
+    //         include: {
+    //             dislikes: true,
+    //             likes: true
+    //         }
+    //     });
+    // }
+
     public async getAllPlaces(): Promise<places[]> {
-        return await prisma.places.findMany({
-            include: {
-                dislikes: true,
-                likes: true
-            }
-        });
+        return await prisma.places.findMany({});
     }
 
-    public async getPlaceById(id_place: number): Promise<places | null> {
-        try {
-            const place = await prisma.places.findUnique({
-                where: {
-                    place_id: id_place
-                },
-                include: {
-                    dislikes: true,
-                    likes: true
-                }
-            });
-            return place;
-        } catch (error) {
-            console.error(error);
-            throw new Error("Error getting place by id");
+    // public async getPlaceById(id_place: number): Promise<places | null> {
+    //     try {
+    //         const place = await prisma.places.findUnique({
+    //             where: {
+    //                 place_id: id_place
+    //             },
+    //             include: {
+    //                 dislikes: true,
+    //                 likes: true
+    //             }
+    //         });
+    //         return place;
+    //     } catch (error) {
+    //         console.error(error);
+    //         throw new Error("Error getting place by id");
 
-        }
-    }
+    //     }
+    // }
 
-    public async getPlacesByPage(per_page: number, page: number): Promise<PlacesResponse | null> {
-        const validPage = Math.max(page, 1); // This is to avoid negative pages and make sure that the page is greater than 0
-        const validPerPage = Math.max(per_page, 1);
-        try {
-            const total_places = await prisma.places.count();
-            if (total_places === 0) {
-                return null;
-            }
-            const places = await prisma.places.findMany({
-                skip: (validPage - 1) * validPerPage,
-                take: validPerPage,
-                include: {
-                    likes: true,
-                    dislikes: true
-                }
-            });
-            return {
-                data: places,
-                current_page: validPage,
-                per_page: validPerPage,
-                total_places: total_places,
-                total_pages: Math.ceil(total_places / validPerPage) // this is to calculate the total number of pages and round it up to the nearest whole number
-            }
-        } catch (error) {
-            console.error(error);
-            throw new Error("Error getting place by id");
+    // public async getPlacesByPage(per_page: number, page: number): Promise<PlacesResponse | null> {
+    //     const validPage = Math.max(page, 1); // This is to avoid negative pages and make sure that the page is greater than 0
+    //     const validPerPage = Math.max(per_page, 1);
+    //     try {
+    //         const total_places = await prisma.places.count();
+    //         if (total_places === 0) {
+    //             return null;
+    //         }
+    //         const places = await prisma.places.findMany({
+    //             skip: (validPage - 1) * validPerPage,
+    //             take: validPerPage,
+    //             include: {
+    //                 likes: true,
+    //                 dislikes: true
+    //             }
+    //         });
+    //         return {
+    //             data: places,
+    //             current_page: validPage,
+    //             per_page: validPerPage,
+    //             total_places: total_places,
+    //             total_pages: Math.ceil(total_places / validPerPage) // this is to calculate the total number of pages and round it up to the nearest whole number
+    //         }
+    //     } catch (error) {
+    //         console.error(error);
+    //         throw new Error("Error getting place by id");
 
-        }
-    }
+    //     }
+    // }
 
-    public async registerPlace(data: Prisma.placesCreateInput): Promise<Place> {
-        return await prisma.places.create({
+    // // public async registerPlace(data: Prisma.placesCreateInput): Promise<Place> {
+    // //     return await prisma.places.create({
+    // //         data: {
+    // //             name: data.name,
+    // //         }
+
+    // //     });
+    // // }
+
+    // public async deletePlace(id_place: number): Promise<Place | null> {
+    //     return await prisma.places.delete({
+    //         where: {
+    //             place_id: id_place
+    //         }
+    //     });
+    // }
+
+    public async createPlace(placeRespone: PlaceResponse): Promise<Place | null> {
+        return prisma.places.create({
             data: {
-                name: data.name,
-            }
-
-        });
-    }
-
-    public async deletePlace(id_place: number): Promise<Place | null> {
-        return await prisma.places.delete({
-            where: {
-                place_id: id_place
+                name: placeRespone.name,
+                google_id: placeRespone.google_id,
+                lat: placeRespone.lat,
+                lng: placeRespone.lng,
+                rating: placeRespone.rating,
+                vicinity: placeRespone.vicinity,
+                photos: placeRespone.photos
             }
         });
     }
