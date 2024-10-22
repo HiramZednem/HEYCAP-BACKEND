88352cef7d1f0213eb2bf004ce4f7b481154e045
HiramZednem <hirammendez000@gmail.com>
2024-10-21 22:15:00 -0600

    ADD: search nearby excludes places rated by user

--- src/controllers/place.controller.ts
+++ src/controllers/place.controller.ts
@@ -3,15 +3,17 @@ import { PlaceService } from "../services/place.service";
 import { BaseResponse } from "../dtos/base.response";
 import { jwtPlugin } from "../public";
 import { tokenService } from "../services/token.service";
-import { GoogleService } from "../services";
+import { GoogleService, InteractionService, userService } from "../services";
 
 export class PlaceController {
     private placeServices: PlaceService;
     private googleServices: GoogleService;
+    private InteractionService: InteractionService;
 
     constructor(){
         this.placeServices = new PlaceService();
         this.googleServices = new GoogleService();
+        this.InteractionService = new InteractionService();
     }
 
 
@@ -57,15 +59,27 @@ export class PlaceController {
         }
 
         // TODO: De los places que me llegaron, voy a filtrar a los que el usuario les dio like o dislike
+        const user = await userService.getById(uuid);
+        const userInteractions = await this.InteractionService.getInteractionsByUserId(user.user_id);
+
+        let placesVisitedByUser = [
+            ...userInteractions.likes.map(like => like.place.google_id),
+            ...userInteractions.dislikes.map(dislike => dislike.place.google_id)
+        ];
+
+        results!.places = results!.places.filter(place => !placesVisitedByUser.includes(place.google_id));
 
         const response = new BaseResponse(results, true, "Nearby places found");
         res.status(200).json(response.toResponseEntity());
-         
 
-        // De los places que me llegaron, voy a filtrar si el usuario les dio like o dislike y esos voy a regresar junto con el next page
-
-        } catch {
-
+        } catch(error: unknown) {
+            if (error instanceof Error) {
+                console.error(error);
+                const response = new BaseResponse({}, false, error.message);
+                res.status(500).json(response.toResponseEntity());
+            } else {
+                return res.status(500).json({ error: 'An unexpected error occurred' });
+            }
         }
         
     }
