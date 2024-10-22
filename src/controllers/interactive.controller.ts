90e5d5bbb03ad59581e7ed86db1d431c54f51105
HiramZednem <hirammendez000@gmail.com>
2024-10-21 20:18:50 -0600

    ADD: functionality for likes and dislikes

--- src/controllers/interactive.controller.ts
+++ src/controllers/interactive.controller.ts
@@ -32,24 +32,46 @@ export class InteractiveController {
             }
 
 
-            const result = this.interactiveService.likeMethod(user.user_id, place.place_id);
-            const response = new BaseResponse(result, true, "Like set");
+            const result = await this.interactiveService.likeMethod(user.user_id, place.place_id);
+            const response = new BaseResponse({}, true, "Like set");
             res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, "Error setting like");
-            res.status(500).json(response.toResponseEntity());
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                const response = new BaseResponse({}, false, error.message);
+                res.status(500).json(response.toResponseEntity());
+            } else {
+                return res.status(500).json({ error: 'An unexpected error occurred' });
+            }
         }
     }
 
     public async setDislike(req: Request, res: Response) {
         try {
-            const { user_id, place_id } = req.body;
-            const result = this.interactiveService.dislikeMethod(parseInt(user_id as string), parseInt(place_id as string));
-            const response = new BaseResponse(result, true, "Dislike set");
+            const accessToken = req.app.locals.accessToken;
+            const uuid = jwtPlugin.decode(accessToken).uuid;
+            await tokenService.validateToken(accessToken, uuid);
+
+            const user = await userService.getById(uuid);
+            const place = await this.placeService.getPlaceById(req.params.id_place);
+
+            if (!user) {
+                throw new Error("User not found");
+            }
+            if (!place) {
+                throw new Error("Place not found");
+            }
+
+
+            const result = await this.interactiveService.dislikeMethod(user.user_id, place.place_id);
+            const response = new BaseResponse({}, true, "Dislike set");
             res.status(200).json(response.toResponseEntity());
-        } catch (error) {
-            const response = new BaseResponse({}, false, "Error setting dislike");
-            res.status(500).json(response.toResponseEntity());
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                const response = new BaseResponse({}, false, error.message);
+                res.status(500).json(response.toResponseEntity());
+            } else {
+                return res.status(500).json({ error: 'An unexpected error occurred' });
+            }
         }
     }
 }
--- src/services/interactive.service.ts
+++ src/services/interactive.service.ts
@@ -6,48 +6,102 @@ export class InteractiveService {
     constructor(){}
 
     public async likeMethod(user_id: number, place_id: number): Promise<likes> {
-        try {
-            const like = await prisma.likes.create({
-                data: {
-                    place: {
-                        connect: {
-                            place_id: place_id
-                        }
-                    },
-                    user: {
-                        connect: {
-                            user_id: user_id
-                        }
+        if (await this.hasUserLikedPlace(user_id, place_id)) {
+            throw new Error('User has already liked this place');
+        }
+
+        if (await this.hasUserDislikedPlace(user_id, place_id)) {
+            await prisma.dislikes.delete({
+                where: { 
+                    user_id_place_id: {
+                        place_id: place_id,
+                        user_id: user_id
+                    }
+                    }
+            })
+        }
+
+
+        const like = await prisma.likes.create({
+            data: {
+                place: {
+                    connect: {
+                        place_id: place_id
                     }
                 },
-            });
-            return like;
-        } catch (error) {
-            console.error(error);
-            throw new Error("Error creating like");
-        }
+                user: {
+                    connect: {
+                        user_id: user_id
+                    }
+                }
+            },
+        });
+        return like;
+        
     }
 
     public async dislikeMethod(user_id: number, place_id: number): Promise<dislikes> {
-        try {
-            const like = await prisma.dislikes.create({
-                data: {
-                    place: {
-                        connect: {
-                            place_id: place_id
-                        }
-                    },
-                    user: {
-                        connect: {
-                            user_id: user_id
-                        }
+        if (await this.hasUserDislikedPlace(user_id, place_id)) {
+            throw new Error('User has already disliked this place');
+        }
+
+        if (await this.hasUserLikedPlace(user_id, place_id)) {
+            await prisma.likes.delete({
+                where: { 
+                    user_id_place_id: {
+                        place_id: place_id,
+                        user_id: user_id
+                    }
+                    }
+            });
+        }
+
+        const like = await prisma.dislikes.create({
+            data: {
+                place: {
+                    connect: {
+                        place_id: place_id
                     }
                 },
-            });
-            return like;
-        } catch (error) {
-            console.error(error);
-            throw new Error("Error creating like");
+                user: {
+                    connect: {
+                        user_id: user_id
+                    }
+                }
+            },
+        });
+        return like;
+    }
+
+    private async hasUserLikedPlace(user_id: number, place_id: number) {
+        const existingLike = await prisma.likes.findFirst({
+            where: {
+                user_id: user_id,
+                place_id: place_id,
+            },
+        });
+    
+        if (existingLike) {
+            // El usuario ya le dio like al lugar
+            return true;
+        } else {
+            // El usuario no le ha dado like al lugar
+            return false;
+        }
+    }
+
+    private async hasUserDislikedPlace(user_id: number, place_id: number) {
+        const existingDislike = await prisma.dislikes.findFirst({
+            where: {
+                user_id: user_id,
+                place_id: place_id,
+            },
+        });
+    
+        if (existingDislike) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
